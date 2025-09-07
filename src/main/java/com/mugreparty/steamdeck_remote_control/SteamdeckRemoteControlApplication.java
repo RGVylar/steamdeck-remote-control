d9be0c487b0b8f21d5d74fd237211d3cdebfb527
RGVylar <rgvylar@gmail.com>
2025-09-07 12:53:21 +0200

    feat: initialzr base app

---
+++ src/main/java/com/mugreparty/steamdeck_remote_control/SteamdeckRemoteControlApplication.java
@@ -0,0 +1,13 @@
+package com.mugreparty.steamdeck_remote_control;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SteamdeckRemoteControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SteamdeckRemoteControlApplication.class, args);
+	}
+
+}
